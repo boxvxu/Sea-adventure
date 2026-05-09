7f76b64b92fe6dab18833eab6f817ceecbe6da2e
boxvxu <mfband814@gmail.com>
2026-05-09 11:57:24 +0100

    final

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,3 +4,7 @@ burger.onclick = () =>{
     burger.classList.toggle("burger-close")
     menu.classList.toggle("menu-open")
 }
+menu.onclick = () =>{
+    burger.classList.remove("burger-close")
+    menu.classList.remove("menu-open")
+}
